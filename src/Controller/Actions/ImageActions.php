8bac38051e4293240014cc942a97ea8732915ebb
Lopo <lopo@losys.eu>
2022-10-25 17:04:19 +0200

    eliminate ImageResponse, use AbstractImagine instead of concrete one

--- src/Controller/Actions/ImageActions.php
+++ src/Controller/Actions/ImageActions.php
@@ -2,12 +2,15 @@
 
 namespace Limas\Controller\Actions;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Gaufrette\Exception\FileNotFound;
-use Imagine\Gd\Imagine;
+use Imagine\Image\AbstractImagine;
 use Imagine\Image\Box;
+use Imagine\Image\Point;
 use Limas\Entity\CachedImage;
 use Limas\Entity\UploadedFile;
-use Limas\Response\ImageResponse;
+use Limas\Service\MimetypeIconService;
+use Limas\Service\UploadedFileService;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,22 +19,35 @@ use Symfony\Component\HttpFoundation\Response;
 class ImageActions
 	extends FileActions
 {
-	public function getImageAction(Request $request, int $id, LoggerInterface $logger): ImageResponse|Response
+	public function __construct(
+		EntityManagerInterface                   $entityManager,
+		UploadedFileService                      $uploadedFileService,
+		MimetypeIconService                      $mimetypeIconService,
+		LoggerInterface                          $logger,
+		array                                    $limas,
+
+		protected readonly AbstractImagine       $liipImagine
+	)
+	{
+		parent::__construct($entityManager, $uploadedFileService, $mimetypeIconService, $logger, $limas);
+	}
+
+	public function getImageAction(Request $request, int $id, LoggerInterface $logger): Response
 	{
 		$image = $this->entityManager->find($this->getEntityClass($request), $id);
 		$width = $request->get('maxWidth', 200);
 		$height = $request->get('maxHeight', 200);
 		if ($image === null) {
-			return new ImageResponse($width, $height, Response::HTTP_NOT_FOUND, '404 not found');
+			return $this->createImageResponse($width, $height, Response::HTTP_NOT_FOUND, '404 not found');
 		}
 		try {
 			$file = $this->fitWithin($image, $width, $height);
 		} catch (FileNotFound $e) {
 			$logger->error($e->getMessage());
-			return new ImageResponse($width, $height, Response::HTTP_NOT_FOUND, '404 not found');
+			return $this->createImageResponse($width, $height, Response::HTTP_NOT_FOUND, '404 not found');
 		} catch (\Exception $e) {
 			$logger->error($e->getMessage());
-			return new ImageResponse($width, $height, Response::HTTP_INTERNAL_SERVER_ERROR, '500 Server Error');
+			return $this->createImageResponse($width, $height, Response::HTTP_INTERNAL_SERVER_ERROR, '500 Server Error');
 		}
 
 		return new Response(file_get_contents($file), Response::HTTP_OK, ['Content-Type' => 'image/png']);
@@ -65,7 +81,8 @@ class ImageActions
 
 		file_put_contents($localCacheFile, $this->uploadedFileService->getStorage($image)->read($image->getFullFilename()));
 
-		(new Imagine)->open($localCacheFile)
+		$this->liipImagine
+			->open($localCacheFile)
 			->thumbnail(new Box($width, $height))
 			->save($outputFile);
 
@@ -90,5 +107,27 @@ class ImageActions
 				->where($queryBuilder->expr()->eq('c.cacheFile', ':file'))
 				->setParameter('file', $this->getImageCacheFilename($image, $width, $height, $mode))
 				->getQuery()->getSingleScalarResult() > 0;
+	}
+
+	private function createImageResponse(int $maxWidth, int $maxHeight, int $code, string $message): Response
+	{
+		if (0 === $maxWidth) {
+			$maxWidth = 300;
+		}
+		if (0 === $maxHeight) {
+			$maxHeight = 300;
+		}
+
+		$image = $this->liipImagine->create(new Box(300, 300));
+		$image->draw()->text($message, $this->liipImagine->font(realpath($this->getParameter('kernel.project_dir') . '/public/fonts/OpenSans-Regular.ttf'), 24, $image->palette()->color('000')), new Point(0, 0));
+
+		$box = $image->getSize();
+		$box = $box->widen($maxWidth);
+		if ($box->getHeight() > $maxHeight) {
+			$box = $box->heighten($maxHeight);
+		}
+		$image->resize($box);
+
+		return new Response($image->get('png'), $code, ['Content-Type' => 'image/png']);
 	}
 }
